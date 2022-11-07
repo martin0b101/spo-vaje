c1433d99b60665449c7f81019d84e7eec34a65d9
martin0b101 <martin.vrbancic@gmail.com>
2022-11-07 17:29:40 +0100

    write it's not working

--- vaja04/naloga1.c
+++ vaja04/naloga1.c
@@ -9,41 +9,29 @@
 #define BUFFER_SIZE 1000
 
 //Writes data from stanadrd input to file.
-int append_text(int* file){
+int append_text(char* filename){
+    FILE* file;
+    file = fopen(filename, "a");
+    int bytes_read = 0;
     char buffer[BUFFER_SIZE];
-    int bytes_read;
     while ((bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE)) > 0)
     {
-        if(write(file, buffer, bytes_read) != bytes_read){
-            printf("error write");
-            return 1;
-        }
+        printf("Bytes read %d", bytes_read);
+        fputc(bytes_read, file);
     }
+    fputs(buffer, file);
+    if (bytes_read < 0)
+    {
+        perror("Napaka branja: ");
+    }
+    fclose(filename);
+    
     return 0;
 }
 
 //Deletes line on number n.
-int delete_n_line(int* file, int* line_number){
-    char buffer[BUFFER_SIZE];
-    int number_of_lines = 0;
-    int start_place;
-    int end_place;
-    int bytes_read;
-    printf("File desc in d: %d\n", file);
-    while ((bytes_read = read(file, buffer, BUFFER_SIZE)) > 0){
-        for (int i = 0; i < bytes_read; i++)
-        {
-            if (buffer[i] == '\n')
-            {
-                printf("\n\nFound new line on byte %d\n\n", i);
-            }
-        }
-    }
-    
-    if (bytes_read == -1){
-        perror("Error reading: ");
-    }
-    printf("Bytes read %d", bytes_read);
+int delete_n_line(FILE* file, int* line_number){
+
     return 0;
 }
 
@@ -55,24 +43,23 @@ int main(int argc, char *argv[]){
         printf("Error: to few arguments!\n");
 
     
-    //open file for writing
+    FILE *file;
+
     //printf("File desc o: %d\n", file);
     //append
     if (argv[2][0] == 'a')
     {
-        int file = open(argv[1], O_WRONLY | O_APPEND | O_RDONLY);
-        append_text(file);
-        if (close(file) > 0)
-            perror("Can't close the file: ");
+        printf("Writing:\n");
+        append_text(argv[1]);
+        //fclose(file);
         /* code */
     }else if(argv[2][0] == 'd'){
         //delete 
-        int file = open(argv[1], O_RDONLY);
+        /*file = fopen(argv[1], "r");
         printf("delete\n");
         int n = atoi(argv[3]);
-        int bytes_read = 0;
-        char buffer[BUFFER_SIZE];        
         delete_n_line(file, n);
+        fclose(file);*/
 
     }else if(argv[2][0] == 'i'){
         //insert
