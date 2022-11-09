451edb486643d77d3c94f3c66cbda9c2efa24043
mvrbancic <martin.vrbancic@cosylab.com>
2022-11-09 23:17:12 +0100

    edo editor and bash sheel working

--- vaja04/naloga1.c
+++ vaja04/naloga1.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define BUFFER_SIZE 1000
 
@@ -24,40 +25,52 @@ int delete_n_line(char* file_name, int* line_number){
     char line[BUFFER_SIZE];
     FILE* file = fopen(file_name, "r");
     FILE* temp = fopen("temp", "w");
-    int counter = 1;
+    int line_counter = 1;
     char buff[BUFFER_SIZE];
-    while(!feof(file)) {
+    bool read = true;
+
+    do{
         fgets(line, BUFFER_SIZE, file);
-        if (counter != line_number){
+        if (feof(file))
+            read = false;
+        else if(line_counter != line_number)
             fputs(line, temp);
-        }
-        counter++;
-    }
-    remove(file_name);
+
+        line_counter++;
+    }while(read);
+
     fclose(temp);
     fclose(file);
+    remove(file_name);
     rename("temp", file_name);
     return 0; 
 
 }
 
-int insert_on_nth_line(char* file_name, int* line_number){
+int insert_on_nth_line(char* file_name, int* line_number, char* newline_content){
     char line[BUFFER_SIZE];
     FILE* file = fopen(file_name, "r");
     FILE* temp = fopen("temp", "w");
-    int counter = 1;
+    int line_counter = 1;
     char buff[BUFFER_SIZE];
-    while(!feof(file)) {
+    bool read = true;
+
+    do{
         fgets(line, BUFFER_SIZE, file);
-        if (counter == line_number){
-            fputs("\n", temp);
+        if (feof(file))
+            read = false;
+        if(line_counter == line_number){
+            fputs(newline_content, temp);
+            fputs(line, temp);
+        }else{
+            fputs(line, temp);
         }
-        fputs(line, temp);
-        counter++;
-    }
-    remove(file_name);
+        line_counter++;
+        
+    }while(read);
     fclose(temp);
     fclose(file);
+    remove(file_name);
     rename("temp", file_name);
     return 0;
 }
@@ -68,10 +81,6 @@ int main(int argc, char *argv[]){
         printf("Error: to much arguments!\n");
     if (argc < 3)
         printf("Error: to few arguments!\n");
-
-    
-
-    //printf("File desc o: %d\n", file);
     //append
     if (argv[2][0] == 'a')
     {
@@ -87,8 +96,11 @@ int main(int argc, char *argv[]){
 
     }else if(argv[2][0] == 'i'){
         //insert
+        char newline[BUFFER_SIZE];
+        printf("insert line: ");
         int n = atoi(argv[3]);
-        insert_on_nth_line(argv[1], n);
+        fgets(newline, BUFFER_SIZE, stdin);
+        insert_on_nth_line(argv[1], n, newline);
     }else{
         printf("Wrong arguments!\n");
     }
--- vaja04/naloga2.c
+++ vaja04/naloga2.c
@@ -1,6 +1,75 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 
+#define err_sys(mess) { fprintf(stderr,"Error: %s.", mess); exit(1); }
+#define MAXLINE 512
 
+struct args {
+  char **args;
+  int argc;
+};
 
-int main(){
-    
+struct args* parse_arguments(char* line){
+    char** args;
+    args = malloc(MAXLINE*sizeof(char*));
+    int counter = 0;
+    int new_arg_started = 0;
+    int in_len = strlen(line);
+    for (int i = 0; i < in_len && i < MAXLINE-1; i++) {
+        if (line[i] == '\n') {
+            line[i] = 0;
+            break;
+        } else if (line[i] == ' ') {
+            if (!new_arg_started) { /* Do nothing (dupliciran space) */ }
+            new_arg_started = 0;
+            line[i] = 0;
+        } else {
+            // vsebina!
+            if (!new_arg_started) {
+                counter++; // nova vsebina
+                args[counter-1] = &line[i];
+            }
+            new_arg_started = 1;
+        }
+    }
+    args[counter+1] = NULL;
+    //printf("im outside of while lop\n");
+    struct args *arguments = malloc(sizeof(struct args));
+    arguments->args = args;
+    arguments->argc = counter;
+
+    return arguments;
+}
+
+int main()
+{
+	char buf[MAXLINE];
+	pid_t pid;
+	int status;
+	printf("> "); // izpiše prompt %
+    struct args *argument_list;
+	while (fgets(buf, MAXLINE, stdin) != NULL)
+	{
+        buf[strlen(buf) - 1] = 0; // spremeni znak za novo vrstico v ničti znak
+        argument_list = parse_arguments(buf);
+        if ( (pid = fork()) < 0)
+			err_sys("fork error")
+		else if (pid == 0) { // otrok
+            char **commands = argument_list->args;
+            int code = execvp(commands[0], commands);
+
+			printf("couldn't execute: %s", buf);
+            free(argument_list);
+			exit(127);
+		}
+		// starš
+		if ( (pid = waitpid(pid, &status, 0)) < 0)
+			err_sys("waitpid error")
+		printf("> ");
+	}
+
+	exit(0);
 }
